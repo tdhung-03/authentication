c019767d0318c122b75a78d64c3bc08be0829aff
tdhung03 <mspotify208@gmail.com>
2023-09-12 16:21:13 +0700

    simple authentication

--- taskmanager/user/views.py
+++ taskmanager/user/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 
-def login(request):
-    context = {
+def login_view(request):
+    username = ""
+    password = ""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        message = 'Successfully logged in'
+    else:
+        message = 'Failed to login'
+    context = {
+        'message': message,
     }
     return render(request, 'user/login.html', context)
