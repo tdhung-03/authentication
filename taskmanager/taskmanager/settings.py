ec6d45482697977a4a6d3d6bcb8bcae5607ee7e6
tdhung03 <mspotify208@gmail.com>
2023-09-12 10:25:09 +0700

    login form

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "task.apps.TaskConfig",
+    "user.apps.UserConfig",
 ]
 
 MIDDLEWARE = [
---
+++ taskmanager/user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def login(request):
+    context = {
+
+    }
+    return render(request, 'user/login.html', context)
